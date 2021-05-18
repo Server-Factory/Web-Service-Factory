cf7793cf012ab46c1877ef02739ed4a9b90e417d
Milos Vasic <milos85vasic@gmail.com>
2021-05-18 18:35:00 +0200

    WSF-1 - More init.

--- Application/src/os/macos/kotlin/net/milosvasic/factory/mail/application/OSInit.kt
+++ Application/src/os/macos/kotlin/net/milosvasic/factory/mail/application/OSInit.kt
@@ -1,6 +1,7 @@
 package net.milosvasic.factory.mail.application
 
 import com.apple.eawt.Application
+import net.milosvasic.factory.log
 import net.milosvasic.factory.platform.OperatingSystem
 import net.milosvasic.factory.platform.Platform
 import net.milosvasic.factory.web_service.application.BuildInfo
@@ -17,6 +18,7 @@ object OSInit : Runnable {
     )
     override fun run() {
 
+        log.v("Starting: ${BuildInfo.versionName}, ${BuildInfo.version}")
         val hostOS = OperatingSystem.getHostOperatingSystem()
         val iconResourceName = "assets/Logo.png"
         val iconResource = hostOS::class.java.classLoader.getResourceAsStream(iconResourceName)
